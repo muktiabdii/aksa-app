86cd7ee545eb5cf5f9a61996b7ae251f99166dbd
muktiabdii <abdisyukur10@gmail.com>
2025-11-13 20:00:22 +0700

    feat: initiating design system

--- app/src/main/java/com/example/aksa/ui/theme/Color.kt
+++ app/src/main/java/com/example/aksa/ui/theme/Color.kt
@@ -9,3 +9,45 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// non white
+val NonWhite = Color(0xFFFCFCFC)
+
+// non-black
+val NonBlack = Color(0xFF030303)
+
+// secondary
+val Sc10 = Color(0xFFFFF9F6)
+val Sc20 = Color(0xFFFFE8DE)
+val Sc30 = Color(0xFFFFD7C5)
+val Sc40 = Color(0xFFFFC5AC)
+val Sc50 = Color(0xFFE2A88F)
+val Sc60 = Color(0xFFC48C74)
+val Sc70 = Color(0xFFA7725B)
+val Sc80 = Color(0xFF8A5A45)
+val Sc90 = Color(0xFF6D4432)
+val Sc100 = Color(0xFF502F21)
+
+// primary
+val Pr10 = Color(0xFFFFF2F2)
+val Pr20 = Color(0xFFFFDBDC)
+val Pr30 = Color(0xFFFFC4C5)
+val Pr40 = Color(0xFFFFADAF)
+val Pr50 = Color(0xFFFF9798)
+val Pr60 = Color(0xFFEE7779)
+val Pr70 = Color(0xFFC54B4D)
+val Pr80 = Color(0xFF9D282A)
+val Pr90 = Color(0xFF741011)
+val Pr100 = Color(0xFF4B0102)
+
+// netral
+val Nr10 = Color(0xFFF8F8F8)
+val Nr20 = Color(0xFFE3E3E3)
+val Nr30 = Color(0xFFCFCFCF)
+val Nr40 = Color(0xFFBBBBBB)
+val Nr50 = Color(0xFFA6A6A6)
+val Nr60 = Color(0xFF929292)
+val Nr70 = Color(0xFF7D7D7D)
+val Nr80 = Color(0xFF696969)
+val Nr90 = Color(0xFF555555)
+val Nr100 = Color(0xFF404040)
